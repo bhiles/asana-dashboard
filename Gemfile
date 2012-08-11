5e12476e2ac1059e5db78b466b53aea5b3fcb5f5
Bennett Hiles <bennetthiles@gmail.com>
2012-08-11 15:37:12 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'jquery-rails'
+gem 'pg'
+gem 'haml'
+gem 'sass'
+
+group :development, :test do
+  gem 'haml-rails'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asana::Application.config.secret_token = 'ab12ba2d7a8843342b8ae71af5960dcce87bbfedbfde10a35d9a6821b9962eec6b3cc24b2d86db8dba46f837a7836b9102a70b90c6ed235beb41cc71d493dd7e'
