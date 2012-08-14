a9cd115d3465e2987b2c0b8288d6cc4335189c6c
Bennett Hiles <bennetthiles@gmail.com>
2012-08-14 00:02:17 -0700

    project dashboard with deliverables and tasks

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,14 +1,6 @@
 class DashboardsController < ApplicationController
   def index
-    all_users = Api::User.all
-    @users = []
-    @users << all_users.pop
-    @users << all_users.pop
-    @users << all_users.pop
-    @users << all_users.pop
-    @users << all_users.pop
-
-    #@tasks = @users.each{|user| Api::Task.currently_working_on(user)}
-    @users.each{|user| user.add_current_task!}
+    @project = Api::Project.find(1410299255548)
+    @project.add_deliverables!
   end
 end
--- app/models/api.rb
+++ app/models/api.rb
@@ -32,15 +32,94 @@ module Api
 
   end
 
+  class Project
+    include ActiveModel::MassAssignmentSecurity
+
+    attr_accessor :id, :name, :deliverables
+
+    def self.find(id)
+      url = "https://app.asana.com/api/1.0/projects/#{id}"
+      private_resource = RestClient::Resource.new url, CONFIG['api_key'], ''
+      response = private_resource.get
+      data = MultiJson.load(response)
+      self.new(data["data"])
+    end
+
+    def initialize(attrs)
+      @id = attrs["id"]
+      @name = attrs["name"]
+    end
+
+    def add_deliverables!
+      self.deliverables = Deliverable.all_for_project_id(self.id)
+    end
+
+  end
+
+  class Deliverable
+    include ActiveModel::MassAssignmentSecurity
+
+    attr_accessor :id, :name, :tasks
+
+    def self.all_for_project_id(project_id)
+      all_tasks = Task.all_for_project_id(project_id)
+      deliverable = empty_deliverable
+      tasks = []
+      deliverables = []
+      all_tasks.each do |single_task|
+        if single_task.is_deliverable?
+          if !tasks.empty?
+            deliverables << self.new(
+                {"id" => deliverable.id,
+                  "name" => deliverable.name,
+                  "tasks" => tasks})
+            tasks = []
+          end
+          deliverable = single_task
+        else
+          tasks << single_task
+        end
+      end
+      if !tasks.empty?
+        deliverables << self.new(
+          {"id" => deliverable.id,
+            "name" => deliverable.name,
+            "tasks" => tasks})
+      end
+      return deliverables
+    end
+
+    def initialize(attrs)
+      @id = attrs["id"]
+      @name = attrs["name"]
+      @tasks = attrs["tasks"]
+    end
+
+
+    def self.empty_deliverable
+      self.new({"id" => 0, "name" => nil})
+    end
+
+  end
+
   class Task
     include ActiveModel::MassAssignmentSecurity
 
     TODAY = "today"
+    DELIVERABLE_SUFFIX = ":"
 
     attr_accessor :id, :name
 
     def self.all(user)
       url = "https://app.asana.com/api/1.0/workspaces/#{CONFIG['workspace_id']}/tasks?assignee=#{user.id}"
+      private_resource = RestClient::Resource.new url, CONFIG['api_key'], ''
+      response = private_resource.get
+      data = MultiJson.load(response)
+      data["data"].map{|u| self.new(u)}
+    end
+
+    def self.all_for_project_id(project_id)
+      url = "https://app.asana.com/api/1.0/projects/#{project_id}/tasks"
       private_resource = RestClient::Resource.new url, CONFIG['api_key'], ''
       response = private_resource.get
       data = MultiJson.load(response)
@@ -65,6 +144,10 @@ module Api
       else
         false
       end
+    end
+
+    def is_deliverable?
+      DELIVERABLE_SUFFIX == self.name[-1]
     end
 
   end
